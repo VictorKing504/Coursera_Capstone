71f33bf86ec046a836ab76b5ac25765754a19b4b
VictorKing <victorking504@yahoo.com>
2020-07-14 14:08:04 -0500

    posting an assignment to my repository

---
+++ Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### <center>Coursera Capstone</center>
+
+# **This is just a test file.The purpose of this is to write a description and write some simple codes. This is the description. The commands are below**
+
+# In[1]:
+
+
+import pandas as py
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project')
+
+
+# In[ ]:
+
+
+
+
